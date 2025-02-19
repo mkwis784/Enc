331cad59eca94df6881614ad6975188ecbf11e76
mkwis784 <mkwis567@gmail.com>
2025-02-19 16:42:12 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+app = Flask(name)
+
+# Define a route for the homepage
+.route('/')
+def hello_world():
+    return 'This bot is made by  and currently it hosted and live for everyone'
+
+# Run the application
+if name == 'main':
+  
+ app.run()
